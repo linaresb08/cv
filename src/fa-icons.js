6dbd4dba2e5a109cc88d95ecb0cdccf61f4309c3
linaresb08 <linaresb08@gmail.com>
2022-04-20 00:17:52 -0400

    Installed fontawesome icons

---
+++ src/fa-icons.js
@@ -0,0 +1,11 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {} from '@fortawesome/free-regular-svg-icons';
+import {} from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+  // Solid
+  faBars,
+  // Regular
+  // Brand
+);
